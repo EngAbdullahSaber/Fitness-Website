293dba34a644e835f78727a0cc82fc9c0a477667
EngAbdullahSaber <79630968+EngAbdullahSaber@users.noreply.github.com>
2021-03-03 23:31:27 +0200

    Fitness Website
    this is the single web page for fitness

---
+++ fitness website/js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+    "use strict";
+     //  calculate carsouel height
+ 
+     $('.se-pre-con').fadeOut(2000);
+     $('html').css("overflow", "visible");
+     
+    var scroll = new SmoothScroll('a[href*="#"]');
+
+    $("body").niceScroll({cursorborder:"",autohidemode:false,cursorcolor:"#013f7e",boxzoom:true});
+
+   
+
+});
